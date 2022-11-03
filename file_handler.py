630c044c4b537abf5357acf8b97797c11af28111
Rinchapooh <sayapov.r@gmail.com>
2022-11-03 18:33:52 +0600

    file_handler.py

--- file_handler.py
+++ file_handler.py
@@ -1,14 +1,17 @@
 import json
-
-x = """{
-    "Name": "Jennifer Smith",
-    "Contact Number": 7867567898,
-    "Email": "jen123@gmail.com",
-    "Hobbies":["Reading", "Sketching", "Horse Riding"]
-    }"""
+import logging
+from types import SimpleNamespace
 
 
-def load_config(filename: str) -> object
+def load_config(filename: str) -> object:
     try:
-        with open(filename, 'r') as f:
-            json.loads(f, object_hook=lambda )
+        with open(filename, 'r') as fb:
+            return json.load(fb, object_hook=lambda d: SimpleNamespace(**d))
+    except FileNotFoundError as e:
+        logging.warning(f'Load config file: {e.strerror}')
+        return
+    except json.decoder.JSONDecodeError as e:
+        logging.warning(f'Parsing config file: {e.msg}')
+        return
+
+
--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import logging
 import socket
 import urllib.request
 
+import file_handler
 
-
-logging.basicConfig(
-    filename='AQG_backup.log',
-    level=logging.DEBUG,
-    format='%(asctime)s:%(levelname)s:%(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S')
+# logging.basicConfig(
+#     filename='AQG_backup.log',
+#     level=logging.DEBUG,
+#     format='%(asctime)s:%(levelname)s:%(message)s',
+#     datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def health_check(hc_key: str):
-
     try:
         urllib.request.urlopen(f'https://hc-ping.com/{hc_key}', timeout=10)
     except socket.error as e:
@@ -22,9 +21,9 @@ def health_check(hc_key: str):
 def main():
     print("From Main")
 
-key_hc = 'f46c0d1a-fb63-4a36-9ed7-f951eea35de9'
 
 if __name__ == '__main__':
-    health_check(hc_key=key_hc)
+    config = file_handler.load_config('config.json')
 
-
+    health_check(hc_key=config.hc_key)
+    print("DONE")
